4d2c2297a0f81939c72679158b8efc595026b380
da <superda1997@gmail.com>
2023-01-13 14:45:41 +0700

    Add project files.

---
+++ TCom/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TCom.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
